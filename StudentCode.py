6c4b76ea8ee0fda779afd6db6742c50c67c85e59
kazawai <bazinmaxime0@gmail.com>
2023-12-01 15:08:43 +0100

    Example resolution of the problem using random hill climbing

--- StudentCode.py
+++ StudentCode.py
@@ -14,7 +14,7 @@ from qiskit.providers.fake_provider import FakeSingaporeV2,FakeWashingtonV2,Fake
 ##-------------------------------------------------------
 ##     Definition de la fonction objetif à minimiser
 ##-------------------------------------------------------
-def fitness(layout):
+def fitness(layout) -> list:
     init_layout={qr[i]:layout[i] for i in range(len(layout))}
     init_layout=Layout(init_layout)
 
@@ -91,6 +91,36 @@ m=backend.num_qubits
 
 ###### Votre code ici
 
+# Depuis ce code, on sait que la solution est de la forme [0,1,2,...,n-1].
+# On peut donc tester la fonction fitness sur cette solution et optimiser son resultat.
+# La metaheuristique ne doit se baser que sur le layout et la fonction fitness.
+import random
+
+layout = list(range(n))
+fitness(layout)
+
+print(f"n={n}, m={m} et fitness_test={fitness(layout)}. Instance {instance_num} ok !")
+
+random.shuffle(layout)
+fitness(layout)
+print(f"n={n}, m={m} et fitness_test={fitness(layout)}. Instance {instance_num} ok !")
+
+def hill_climbing(layout):
+    best_fitness = fitness(layout)
+    best_layout = layout
+    for i in range(10):
+        new_layout = list(layout)
+        random.shuffle(new_layout)
+        new_fitness = fitness(new_layout)
+        if new_fitness < best_fitness:
+            best_fitness = new_fitness
+            best_layout = new_layout
+    return best_layout
+
+best_layout = hill_climbing(layout)
+print(f"{best_layout}")
+print(f"n={n}, m={m} et fitness_test={fitness(best_layout)}. Instance {instance_num} ok !")
+
 ###### A faire : un algo d'optimisation qui minimise la fonction fitness,
 ###### fonction qui accepte en entrée :
 ###### une liste de n parmi m (n<=m) entiers deux à deux distincts
@@ -103,17 +133,21 @@ m=backend.num_qubits
 ###### qasmfile=f"./Instances/{l.rstrip()}.qasm". Cela devrait résoudre le problème.
 
 ###### Voici un test (à supprimer !) pour s'assurer que tout va bien
-for i in range(1,10):
-    instance_num=i     #### Entre 1 et 9 inclue
-
-    backend_name,circuit_type,num_qubit=instance_selection(instance_num)
-    backend,qc,qr=instance_characteristic(backend_name,circuit_type,num_qubit)
-
-    n=num_qubit
-    m=backend.num_qubits
-    r=fitness(list(range(n)))
-    print(f"n={n}, m={m} et fitness_test={r}. Instance {instance_num} ok !")
-
-
-
+# for i in range(1,10):
+#     instance_num=i     #### Entre 1 et 9 inclue
+#
+#     backend_name,circuit_type,num_qubit=instance_selection(instance_num)
+#     backend,qc,qr=instance_characteristic(backend_name,circuit_type,num_qubit)
+#
+#     print(f"Instance {instance_num} : {backend_name}, {circuit_type}, {num_qubit} qubits")
+#     print(f"qc : {qc.draw()}")
+#     print(f"qr : {qr}")
+#
+#     n=num_qubit
+#     m=backend.num_qubits
+#     r=fitness(list(range(n)))
+#     print(f"n={n}, m={m} et fitness_test={r}. Instance {instance_num} ok !")
+#
+#
+#
 
